5d94d76156aedcc5b46ef92a9f1b4d05c30536df
Alexander D'Este <alexanderdeste@gmail.com>
2017-01-24 22:51:52 +0100

    move dummy data to separate file

--- src/App.js
+++ src/App.js
@@ -5,42 +5,14 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import Maps from './components/Map';
 import AddDebt from './components/AddDebt';
+import data from './dummydata';
 
 import { geolocationFallback } from './api/maps';
 
 class App extends Component {
   state = {
     route: 'home',
-    data: [
-      {
-        _id: 1,
-        name: 'Mimi Lundberg',
-        price: 334e4,
-        address: 'Nybergsgatan 6B 1202, 11445 STOCKHOLM',
-        location: { lat: 59.33711599999999, lng: 18.0844779 },
-      },
-      {
-        _id: 2,
-        name: 'Alexander Deste',
-        price: 105e2,
-        address: 'Kämnärsvägen 67Y, 22646 LUND',
-        location: { lat: 55.7229434, lng: 13.2209822 },
-      },
-      {
-        _id: 3,
-        name: 'Patric Keisala',
-        price: 196e4,
-        address: 'Sävstaholmsgatan 15, 21224 MALMÖ',
-        location: { lat: 55.6133822, lng: 13.045723 },
-      },
-      {
-        _id: 4,
-        name: 'Sandra Filipsson',
-        price: 137e5,
-        address: 'Åkersnäsgatan 1B, 21236 MALMÖ',
-        location: { lat: 55.5875923, lng: 13.0951409 },
-      },
-    ],
+    data,
   };
 
   changeRoute(route) {
@@ -117,7 +89,7 @@ class App extends Component {
                 price={`${list.price} kr`}
               />
             </Tappable>
-            ))
+          ))
         }
         {this.state.route === 'add' && <AddDebt />}
         <Footer

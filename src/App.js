a9072748ea6393b6b0ab834e188b36de7bc484ea
Alexander D'Este <alexanderdeste@gmail.com>
2017-01-24 08:42:37 +0100

    increase readability

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import Listitem from './components/Listitem';
 
 class App extends Component {
- state = {
-   data: [
-     { _id: 1, name: 'Mimi Lundberg', price: 33400, adress: 'Nybergsgatan 6B 1202, 11445 STOCKHOLM' },
-     { _id: 2, name: 'Alexander Deste', price: 10500, adress: 'Kämnärsvägen 67Y, 22646 LUND' },
-     { _id: 3, name: 'Patric Keisala', price: 19600, adress: 'Sävstaholmsgatan 15, 21224 MALMÖ' },
-     { _id: 4, name: 'Sandra Filipsson', price: 13700, adress: 'Åkersnäsgatan 1B, 21236 MALMÖ' },
-     ]
- }
+  state = {
+    data: [
+      { _id: 1, name: 'Mimi Lundberg', price: 334e4, adress: 'Nybergsgatan 6B 1202, 11445 STOCKHOLM' },
+      { _id: 2, name: 'Alexander Deste', price: 105e2, adress: 'Kämnärsvägen 67Y, 22646 LUND' },
+      { _id: 3, name: 'Patric Keisala', price: 196e4, adress: 'Sävstaholmsgatan 15, 21224 MALMÖ' },
+      { _id: 4, name: 'Sandra Filipsson', price: 137e5, adress: 'Åkersnäsgatan 1B, 21236 MALMÖ' },
+      ]
+  }
+
   render() {
     return (
       <div>
-        {this.state.data.map(list => <Listitem name={list.name} adress={list.adress} price={`${list.price} kr`}/>)}
+        {
+          this.state.data.map(list => (
+            <Listitem
+              name={list.name}
+              adress={list.adress}
+              price={`${list.price} kr`}
+            />
+          ))
+        }
       </div>
     );
   }

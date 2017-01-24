8b8daba8aebdddc40c2f94b236c0035be4432c9c
Alexander D'Este <alexanderdeste@gmail.com>
2017-01-24 19:13:59 +0100

    include location in dummy data

--- src/App.js
+++ src/App.js
@@ -9,15 +9,39 @@ class App extends Component {
   state = {
     route: 'home',
     data: [
-      { _id: 1, name: 'Mimi Lundberg', price: 334e4, address: 'Nybergsgatan 6B 1202, 11445 STOCKHOLM' },
-      { _id: 2, name: 'Alexander Deste', price: 105e2, address: 'Kämnärsvägen 67Y, 22646 LUND' },
-      { _id: 3, name: 'Patric Keisala', price: 196e4, address: 'Sävstaholmsgatan 15, 21224 MALMÖ' },
-      { _id: 4, name: 'Sandra Filipsson', price: 137e5, address: 'Åkersnäsgatan 1B, 21236 MALMÖ' },
-      ]
-  }
+      {
+        _id: 1,
+        name: 'Mimi Lundberg',
+        price: 334e4,
+        address: 'Nybergsgatan 6B 1202, 11445 STOCKHOLM',
+        location: { lat: 59.33711599999999, lng: 18.0844779 },
+      },
+      {
+        _id: 2,
+        name: 'Alexander Deste',
+        price: 105e2,
+        address: 'Kämnärsvägen 67Y, 22646 LUND',
+        location: { lat: 55.7229434, lng: 13.2209822 },
+      },
+      {
+        _id: 3,
+        name: 'Patric Keisala',
+        price: 196e4,
+        address: 'Sävstaholmsgatan 15, 21224 MALMÖ',
+        location: { lat: 55.6133822, lng: 13.045723 },
+      },
+      {
+        _id: 4,
+        name: 'Sandra Filipsson',
+        price: 137e5,
+        address: 'Åkersnäsgatan 1B, 21236 MALMÖ',
+        location: { lat: 55.5875923, lng: 13.0951409 },
+      },
+    ],
+  };
 
   changeRoute(route) {
-    this.setState({route})
+    this.setState({ route });
   }
 
   render() {
@@ -27,10 +51,11 @@ class App extends Component {
         <Maps
           width={'100vw'}
           height={'6rem'}
-          center={{lat: 59.955413, lng: 30.337844}}
-          markers={[{lat: 59.955413, lng: 30.337844}]}
+          center={{ lat: 59.955413, lng: 30.337844 }}
+          markers={[ { lat: 59.955413, lng: 30.337844 } ]}
         />
-        {this.state.route === 'home' &&
+        {
+          this.state.route === 'home' &&
           this.state.data.map((list, i) => (
             <Listitem
               key={i}
@@ -40,18 +65,17 @@ class App extends Component {
             />
           ))
         }
-        {this.state.route === 'add' &&
-          <AddDebt />
-        }
+        {this.state.route === 'add' && <AddDebt />}
         <Footer
           onTap={route => this.changeRoute(route)}
-          routes={[{text: 'Add stuff', route: 'add'},
-            {text: 'Browse', route: 'home'}]}
+          routes={[
+            { text: 'Add stuff', route: 'add' },
+            { text: 'Browse', route: 'home' },
+          ]}
           hide={this.state.route}
         />
       </div>
     );
   }
 }
-
 export default App;

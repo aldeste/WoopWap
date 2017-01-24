6f31f4ebb44820d8bcf6c92f1910413a6f925ccc
Sandra Filipsson <ardnassandra13@gmail.com>
2017-01-24 09:52:14 +0100

    styling

--- src/components/Listitem/index.js
+++ src/components/Listitem/index.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import style from './style.css';
 
+
 class Listitem extends Component {
   render() {
     return (
       <div className={style.item}>
         <p>{this.props.adress}</p><p>{this.props.price}</p>
       </div>
+      
     );
   }
 }

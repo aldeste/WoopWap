06dc8a83ef2ef2bfe820d9fcb20c944832cf9337
Alexander D'Este <alexanderdeste@gmail.com>
2017-01-24 11:49:01 +0100

    forms are managed by state

--- src/components/AddDebt/index.js
+++ src/components/AddDebt/index.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import style from './style.css';
 
 class AddDebt extends Component {
+  state = {
+    form: {
+      idNumber: '',
+      name: '',
+      email: '',
+      endDate: '',
+      amount: '',
+      reward: '',
+    }
+  }
+  handleInputChange(event) {
+    const { target } = event;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const { name } = target;
+
+    this.setState({ [name]: value });
+  }
+
   render() {
     return (
       <form className={style.form}>
@@ -9,20 +27,65 @@ class AddDebt extends Component {
           Are you representing a person or an organization?
         </p>
         <div className={style.booleanWrapper}>
-            <label for="com">Organisation</label>
-            <input type="checkbox" name="company" id="com" value="Org" />
-            <label for="per">Person</label>
-            <input type="checkbox" name="person" id="per" value="Person" />
+          <label htmlFor="com">Organisation</label>
+          <input type="checkbox" name="company" id="com" value="Org" />
+          <label htmlFor="per">Person</label>
+          <input type="checkbox" name="person" id="per" value="Person" />
         </div>
-        <input className={style.input} type="text" name="idNumber" placeholder="Org/Personal ID Number" required />
-        <input className={style.input} type="text" name="name" placeholder="Name" />
-        <input className={style.input} type="email" name="email" placeholder="Email" required />
-
+        <input
+          className={style.input}
+          type="text"
+          name="idNumber"
+          placeholder="Org/Personal ID Number"
+          value={this.state.idNumber}
+          onChange={() => this.handleInputChange}
+          required
+        />
+        <input
+          className={style.input}
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={() => this.handleInputChange}
+        />
+        <input
+          className={style.input}
+          type="email"
+          name="email"
+          placeholder="Email"
+          required
+          value={this.state.email}
+          onChange={() => this.handleInputChange}
+        />
         <p>Debt</p>
-        <input className={style.input} type="date" name="startDate" placeholder="Loan startdate" required />
-        <input className={style.input} type="date" name="endDate" placeholder="Loan duedate" required />
-        <input className={style.input} type="text" name="amount" placeholder="Amount" required />
-        <input className={style.input} type="text" name="reward" placeholder="Reward %" required/>
+        <input
+          className={style.input}
+          type="date"
+          name="endDate"
+          placeholder="Loan duedate"
+          required
+          value={this.state.endDate}
+          onChange={() => this.handleInputChange}
+        />
+        <input
+          className={style.input}
+          type="text"
+          name="amount"
+          placeholder="Amount"
+          required
+          value={this.state.amount}
+          onChange={() => this.handleInputChange}
+        />
+        <input
+          className={style.input}
+          type="text"
+          name="reward"
+          placeholder="Reward %"
+          required
+          value={this.state.reward}
+          onChange={() => this.handleInputChange}
+        />
         <input className={style.submit} type="submit" value="Submit" />
       </form>
     );

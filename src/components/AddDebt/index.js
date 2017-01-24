527ce8a7854a6482d0cd78503bb991c2f4c23245
Mimi Lundberg <mimilundberg@Mimis-MacBook-Pro-2.local>
2017-01-24 12:06:51 +0100

    Styling form

--- src/components/AddDebt/index.js
+++ src/components/AddDebt/index.js
@@ -8,21 +8,44 @@ class AddDebt extends Component {
         <p>
           Are you representing a person or an organization?
         </p>
-        <div className={style.booleanWrapper}>
+        <div className={style.checkboxWrapper}>
+          <div className={style.box}>
             <label for="com">Organisation</label>
             <input type="checkbox" name="company" id="com" value="Org" />
+          </div>
+          <div className={style.box}>
             <label for="per">Person</label>
             <input type="checkbox" name="person" id="per" value="Person" />
+          </div>
         </div>
-        <input className={style.input} type="text" name="idNumber" placeholder="Org/Personal ID Number" required />
-        <input className={style.input} type="text" name="name" placeholder="Name" />
-        <input className={style.input} type="email" name="email" placeholder="Email" required />
 
-        <p>Debt</p>
-        <input className={style.input} type="date" name="startDate" placeholder="Loan startdate" required />
-        <input className={style.input} type="date" name="endDate" placeholder="Loan duedate" required />
-        <input className={style.input} type="text" name="amount" placeholder="Amount" required />
-        <input className={style.input} type="text" name="reward" placeholder="Reward %" required/>
+        <label className={style.label}>Org/Personal ID Number</label>
+        <input className={style.input} type="text" name="idNumber" required />
+
+        <label className={style.label}>Name</label>
+        <input className={style.input} type="text" name="name" />
+
+        <label className={style.label}>Email</label>
+        <input className={style.input} type="email" name="email" required />
+
+        <label className={style.label}>Borrowes Name</label>
+        <input className={style.input} type="text" name="startDate" required />
+
+        <label className={style.label}>Borrowes Adress</label>
+        <input className={style.input} type="text" name="startDate" required />
+
+        <label className={style.label}>Loan startdate</label>
+        <input className={style.input} type="date" name="startDate" required />
+
+        <label className={style.label}>Loan enddate</label>
+        <input className={style.input} type="date" name="endDate" required />
+
+        <label className={style.label}>Amount</label>
+        <input className={style.input} type="text" name="amount" required />
+
+        <label className={style.label}>Reward %</label>
+        <input className={style.input} type="text" name="reward" required/>
+
         <input className={style.submit} type="submit" value="Submit" />
       </form>
     );
